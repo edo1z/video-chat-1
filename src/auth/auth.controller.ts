9b1a12ffde9c59f29b693f0c7a8b9796853092a0
web3ten0 <web3ten0@gmail.com>
2023-04-15 00:18:54 +0900

    Added Register Screen

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,22 @@
-import { Controller, Get, Post, Render, Req, Res } from '@nestjs/common';
+import { Controller, Get, Post, Render, Req, Res, Body } from '@nestjs/common';
 import { Request, Response } from 'express';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
+  @Get('register')
+  @Render('register')
+  getRegister() {
+    return {};
+  }
+
+  @Post('register')
+  postRegister(@Body() createUserDto: CreateUserDto) {
+    // ユーザー登録の処理を実装
+    // 登録が成功したらホーム画面にリダイレクト
+    // 失敗したらエラーメッセージを表示
+  }
+
   @Get('login')
   @Render('login')
   getLogin() {
